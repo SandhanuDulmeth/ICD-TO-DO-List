976153679b5e5d1fe0c946ec50eaa9edd4374958
SandhanuDulmeth <sandhanu12@gmail.com>
2024-11-12 13:09:53 +0530

    Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+let row = document.getElementById("row")
+
+const taskArray=[];
+
+
+
+function addTask() {
+    
+    
+    let taskName = document.getElementById("taskName").value
+    
+    console.clear();
+    
+    let rowtask = `<div class="row">
+                <div class="col">
+                    <Label style="font-size: 40px;" for="${taskName}"><pre>${taskName}</pre></Label>
+                </div>
+                <div class="col">
+                    <input type="checkbox" id="${taskName}" name="" style="width: 50px; height: 50px;">
+                </div>
+                </div>`
+    taskArray.push(rowtask);
+
+                
+
+     clearTxt();            
+     loadTasks();
+
+
+
+}
+
+
+function loadTasks(){
+    
+    let rowBody = "";
+    
+    taskArray.forEach(task=>{
+        rowBody+=task;
+    });
+    
+   console.log(rowBody);
+   
+
+    row.innerHTML = rowBody;
+}
+
+
+
+function clearTxt(){
+    document.getElementById("taskName").value = '';
+}
