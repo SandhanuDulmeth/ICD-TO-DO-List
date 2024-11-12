9662f9bffed7279ab7d0e5ab09082ed14e231cba
SandhanuDulmeth <sandhanu12@gmail.com>
2024-11-12 14:25:28 +0530

    Commit

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 let row = document.getElementById("row")
 
 const taskArray=[];
+const taskNameArray=[];
 
+const ischeckedArray=[];
 
 
 function addTask() {
     
     
     let taskName = document.getElementById("taskName").value
-    
+   
     console.clear();
     
     let rowtask = `<div class="row">
@@ -16,9 +18,13 @@ function addTask() {
                     <Label style="font-size: 40px;" for="${taskName}"><pre>${taskName}</pre></Label>
                 </div>
                 <div class="col">
-                    <input type="checkbox" id="${taskName}" name="" style="width: 50px; height: 50px;">
+                    <input type="checkbox" id="${taskName}" name="" style="width: 50px; height: 50px;" >
                 </div>
                 </div>`
+
+    taskNameArray.push(taskName);
+    
+
     taskArray.push(rowtask);
 
                 
@@ -26,14 +32,42 @@ function addTask() {
      clearTxt();            
      loadTasks();
 
-     console.log(document.getElementById(taskName).checked); 
+for (let index = 0; index < taskNameArray.length; index++) {
+   
+   let taskName=taskNameArray[index]
+    console.log(document.getElementById(taskName).checked); 
     document.getElementById(taskName).addEventListener("change",function(){
         if(this.checked){
             console.log("Checked");
+          
+             taskArray[index]=`<div class="row">
+                <div class="col">
+                    <Label style="font-size: 40px;" for="${taskName}"><pre>${taskName}</pre></Label>
+                </div>
+                <div class="col">
+                    <input type="checkbox" id="${taskName}" name="" style="width: 50px; height: 50px;" checked>
+                </div>
+                </div>`;
+           
         }else{
             console.log("unChecked");
+            taskArray[index]=`<div class="row">
+                <div class="col">
+                    <Label style="font-size: 40px;" for="${taskName}"><pre>${taskName}</pre></Label>
+                </div>
+                <div class="col">
+                    <input type="checkbox" id="${taskName}" name="" style="width: 50px; height: 50px;" >
+                </div>
+                </div>`;
         }
     })
+   
+    
+}
+
+     
+
+
 
 
 }
@@ -45,6 +79,9 @@ function loadTasks(){
     
     taskArray.forEach(task=>{
         rowBody+=task;
+
+        
+
     });
     
    console.log(rowBody);
